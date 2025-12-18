8beda8ac0fac653ae50bda67cb68110773753f7b
abir cherif <abir.cherif98@gmail.com>
2025-12-18 18:23:35 +0100

    Update web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -43,7 +43,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "15a4afd45f87938179c2dfb69cb00b69
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ddacee248af9901b640fb00fa36d8afd",
+"flutter_bootstrap.js": "c1d717fdce81ff694878852222180476",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
